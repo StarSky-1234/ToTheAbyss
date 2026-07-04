7c13bbbdb7ece50ea94e816a3a8fd463973b1fc9
StarSky-1234 <zxlailww@163.com>
2026-07-04 21:39:46 +0800

    feat: 同步地图资源

---
+++ shares/types/GameAssets.d.ts
@@ -0,0 +1,38 @@
+// 可在命令行执行【apc resource】重新同步资源
+// 生成时间: 2026年7月4日星期六 GMT+8 21:19:02
+
+/**
+ * @description [ID:100572539]地图音频资源
+ * 共 38 个资源
+ */
+type GameAudioAssets = 'audio/acceleration.mp3' | 'audio/airhorn.mp3' | 'audio/boo.mp3' | 'audio/boy_hurt.mp3' | 'audio/break_block.mp3' | 'audio/bubbles.mp3' | 'audio/death.mp3' | 'audio/dive.mp3' | 'audio/door_close.mp3' | 'audio/door_open.mp3' | 'audio/double_jump.mp3' | 'audio/drama.mp3' | 'audio/electric.mp3' | 'audio/elephant.mp3' | 'audio/explode.mp3' | 'audio/footstep.mp3' | 'audio/girl_hurt.mp3' | 'audio/gunshot.mp3' | 'audio/hit.mp3' | 'audio/jump.mp3' | 'audio/land.mp3' | 'audio/laugh.mp3' | 'audio/place_block.mp3' | 'audio/player_spawn.mp3' | 'audio/punch.mp3' | 'audio/rain.mp3' | 'audio/reach_goal.mp3' | 'audio/reach_record_point.mp3' | 'audio/rooster.mp3' | 'audio/sad_trombone.mp3' | 'audio/scream.mp3' | 'audio/send_chat.mp3' | 'audio/splash.mp3' | 'audio/swim.mp3' | 'audio/sword1.mp3' | 'audio/sword2.mp3' | 'audio/time_start.mp3' | 'audio/wolf.mp3' ;
+
+/**
+ * @description [ID:100572539]地图图片资源
+ * 共 0 个资源
+ */
+type GamePictureAssets = never;
+
+/**
+ * @description [ID:100572539]地图模型资源
+ * 共 0 个资源
+ */
+type GameModelAssets = never;
+
+/**
+ * @description [ID:100572539]地图商城资源
+ * 共 0 个资源
+ */
+type GameProductAssets = never;
+
+/**
+ * @description [ID:100572539]地图UI屏幕名称
+ * 共 1 个资源
+ */
+type UiScreenName = 'screen';
+
+/**
+ * @description [ID:100572539]地图UI元素路径
+ * 共 0 个资源
+ */
+type UiNodePath = never;
